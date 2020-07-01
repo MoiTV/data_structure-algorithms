a380fe06707fe201b68f94800c7794d2e2b987aa
Moises Valdez <moisesvaldez075@gmail.com>
2020-07-01 10:11:49 -0700

    truncate String algorithm

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-class Animal {
-    constructor(type, color, weight, legs) {
-        this.type = type;
-        this.color = color;
-        this.weight = weight;
-        this.legs = legs;
-    }
-
-    speak(voice) {
-        return voice;
+function truncateString(str, num) {
+    if (str.length > num) {
+        return str.slice(0, num) + '...';
+    } else {
+        return str;
     }
 }
+
+console.log(truncateString('A-tisket a-tasket A green and yellow basket', 8));
